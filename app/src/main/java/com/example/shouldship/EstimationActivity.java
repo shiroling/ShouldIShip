5afb5201c839ec6c547d2befbf4e4307267a91b9
NoaDespaux <noa.despaux@etu.iut-tlse3.fr>
2023-03-24 11:21:28 +0100

    activity estimation

---
+++ app/src/main/java/com/example/shouldship/EstimationActivity.java
@@ -0,0 +1,15 @@
+package com.example.shouldship;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EstimationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estimation);
+    }
+
+}
